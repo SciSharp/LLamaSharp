18462beb3132c7c37cd2a695a14855e359be139b
Martin Evans <martindevans@gmail.com>
2023-07-20 16:41:19 +0100

    - Removed the `Update` and `GetOrDefault` extension methods (they were unused).  - Renamed `DictionaryExtensions` to `KeyValuePairExtensions`, since nothing in that file extends dictionary any more!

---
+++ LLama/Extensions/KeyValuePairExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LLama.Extensions
+{
+    /// <summary>
+    /// Extensions to the KeyValuePair struct
+    /// </summary>
+    public static class KeyValuePairExtensions
+    {
+        /// <summary>
+        /// Deconstruct a KeyValuePair into it's constituent parts.
+        /// </summary>
+        /// <param name="pair">The KeyValuePair to deconstruct</param>
+        /// <param name="first">First element, the Key</param>
+        /// <param name="second">Second element, the Value</param>
+        /// <typeparam name="TKey">Type of the Key</typeparam>
+        /// <typeparam name="TValue">Type of the Value</typeparam>
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> pair, out TKey first, out TValue second)
+        {
+            first = pair.Key;
+            second = pair.Value;
+        }
+    }
+}
