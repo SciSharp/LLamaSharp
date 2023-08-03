b8bb5aec92bc1c1e3c3dbae25218ff48b7612639
sa_ddam213 <sa_ddam213@live.com>
2023-08-03 20:21:11 +1200

    LLamaModel to track LLamaModelContext instances

--- LLama/LLamaModel.cs
+++ LLama/LLamaModel.cs
@@ -1,17 +1,8 @@
-﻿using LLama.Exceptions;
+﻿using LLama.Common;
 using LLama.Native;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
-using System.IO.MemoryMappedFiles;
-using LLama.Common;
-using System.Runtime.InteropServices;
-using LLama.Extensions;
-using Microsoft.Win32.SafeHandles;
-using System.Reflection;
 using System.Collections.Concurrent;
+using System.Threading.Tasks;
 
 namespace LLama
 {
@@ -20,6 +11,7 @@ namespace LLama
         // TODO: expose more properties.
         ILLamaLogger? _logger;
         SafeLlamaModelHandle _model;
+        ConcurrentDictionary<string, LLamaModelContext> _contexts;
         /// <summary>
         /// The model params set for this model.
         /// </summary>
@@ -39,19 +31,69 @@ namespace LLama
         public LLamaModel(ModelParams modelParams, ILLamaLogger? logger = null)
         {
             _logger = logger;
-            Params = modelParams;
+            _contexts = new ConcurrentDictionary<string, LLamaModelContext>();
             _logger?.Log(nameof(LLamaModelContext), $"Initializing LLama model with params: {modelParams}", ILLamaLogger.LogLevel.Info);
 
+            Params = modelParams;
             var contextParams = Utils.CreateContextParams(modelParams);
             _model = SafeLlamaModelHandle.LoadFromFile(modelParams.ModelPath, contextParams);
             if (!string.IsNullOrEmpty(modelParams.LoraAdapter))
                 _model.ApplyLoraFromFile(modelParams.LoraAdapter, modelParams.LoraBase, modelParams.Threads);
         }
 
+        /// <summary>
+        /// Creates a new context session on this model
+        /// </summary>
+        /// <param name="contextId">The unique context identifier</param>
+        /// <param name="encoding">The contexts text encoding</param>
+        /// <returns>LLamaModelContext for this LLamaModel</returns>
+        /// <exception cref="Exception">Context exists</exception>
+        public Task<LLamaModelContext> CreateContext(string contextId, string encoding = "UTF-8")
+        {
+            if (_contexts.TryGetValue(contextId, out var context))
+                throw new Exception($"Context with id {contextId} already exists.");
+
+            context = new LLamaModelContext(this, encoding, _logger);
+            if (_contexts.TryAdd(contextId, context))
+                return Task.FromResult(context);
+
+            return Task.FromResult<LLamaModelContext>(null);
+        }
+
+        /// <summary>
+        /// Get a contexts belonging to this model
+        /// </summary>
+        /// <param name="contextId">The unique context identifier</param>
+        /// <returns>LLamaModelContext for this LLamaModel with the specified contextId</returns>
+        public Task<LLamaModelContext> GetContext(string contextId)
+        {
+            if (_contexts.TryGetValue(contextId, out var context))
+                return Task.FromResult(context);
+         
+            return Task.FromResult<LLamaModelContext>(null);
+        }
+
+        /// <summary>
+        /// Remove a context from this model
+        /// </summary>
+        /// <param name="contextId">The unique context identifier</param>
+        /// <returns>true if removed, otherwise false</returns>
+        public Task<bool> RemoveContext(string contextId)
+        {
+            if (!_contexts.TryRemove(contextId, out var context))
+                return Task.FromResult(false);
+
+            context?.Dispose();
+            return Task.FromResult(true);
+        }
 
         /// <inheritdoc />
         public virtual void Dispose()
         {
+            foreach (var context in _contexts.Values)
+            {
+                context?.Dispose();
+            }
             _model.Dispose();
         }
     }
