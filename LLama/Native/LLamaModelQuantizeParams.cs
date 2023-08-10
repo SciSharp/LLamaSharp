acd91341e6fd3733b41a59818b9e5ed078a2c5ac
Martin Evans <martindevans@gmail.com>
2023-08-10 02:14:21 +0100

    Added lots of comments to all the LLamaFtype variants

--- LLama/Native/LLamaModelQuantizeParams.cs
+++ LLama/Native/LLamaModelQuantizeParams.cs
@@ -1,25 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Text;
+﻿using System.Runtime.InteropServices;
 
 namespace LLama.Native
 {
+    /// <summary>
+    /// Quantizer parameters used in the native API
+    /// </summary>
     public struct LLamaModelQuantizeParams
     {
         /// <summary>
         /// number of threads to use for quantizing, if &lt;=0 will use std::thread::hardware_concurrency()
         /// </summary>
         public int nthread;
+
         /// <summary>
         /// quantize to this llama_ftype
         /// </summary>
         public LLamaFtype ftype;
+
         /// <summary>
         /// allow quantizing non-f32/f16 tensors
         /// </summary>
         [MarshalAs(UnmanagedType.I1)]
         public bool allow_requantize;
+
         /// <summary>
         /// quantize output.weight
         /// </summary>
