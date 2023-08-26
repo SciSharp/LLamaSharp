cf4754db44876d8614f55568127fb6eedb97cd07
Martin Evans <martindevans@gmail.com>
2023-08-26 21:38:24 +0100

    Removed unnecessary parameters from some low level sampler methods

---
+++ LLama/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace LLama.Extensions
+{
+    internal static class DictionaryExtensions
+    {
+#if NETSTANDARD2_0
+        public static TValue GetValueOrDefault<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+        {
+            return dictionary.TryGetValue(key, out var value) ? value : defaultValue;
+        }
+#endif
+    }
+}
