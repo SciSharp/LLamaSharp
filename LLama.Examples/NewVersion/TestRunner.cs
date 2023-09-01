521f068d640ecc7a3fbf0605c6041e9ccfd465b8
Tim Miller <innerlogic4321@gmail.com>
2023-09-01 22:42:13 +0900

    Add Embedding for Semantic Kernel

--- LLama.Examples/NewVersion/TestRunner.cs
+++ LLama.Examples/NewVersion/TestRunner.cs
@@ -20,6 +20,8 @@
             Console.WriteLine("10: Constrain response to json format using grammar.");
             Console.WriteLine("11: Semantic Kernel Prompt.");
             Console.WriteLine("12: Semantic Kernel Chat.");
+            Console.WriteLine("13: Semantic Kernel Memory.");
+            Console.WriteLine("14: Semantic Kernel Memory Skill.");
 
             while (true)
             {
@@ -77,6 +79,14 @@
                 else if (choice == 12)
                 {
                     await SemanticKernelChat.Run();
+                }
+                else if (choice == 13)
+                {
+                    await SemanticKernelMemory.Run();
+                }
+                else if (choice == 14)
+                {
+                    await SemanticKernelMemorySkill.Run();
                 }
                 else
                 {
