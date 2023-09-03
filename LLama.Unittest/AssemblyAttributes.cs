21cbecb82d9873002ef884874830089fadf1209b
Martin Evans <martindevans@gmail.com>
2023-09-03 23:35:53 +0100

    Disable test parallelism to prevent fix CI

---
+++ LLama.Unittest/AssemblyAttributes.cs
@@ -0,0 +1,5 @@
+﻿
+// Disable running tests in parallel. Some tests load language models and try to and
+// this can cause a lockup due to memory thrashing and terrible performance.
+// e.g. 7GB model loaded by 10 different tests and trying to evaluate prompts
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
