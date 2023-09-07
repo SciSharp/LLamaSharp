61f397ff8dbd085439dac29a0882fb24405c01a9
Martin Evans <martindevans@gmail.com>
2023-09-07 01:23:25 +0100

    Added the low level bindings to beam search.

---
+++ LLama/Native/LLamaBeamView.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LLama.Native;
+
+using llama_token = Int32;
+
+/// <summary>
+/// Information about a single beam in a beam search
+/// </summary>
+[StructLayout(LayoutKind.Sequential)]
+public struct LLamaBeamView
+{
+    private readonly unsafe llama_token* tokens;
+    private readonly nint n_tokens;
+
+    /// <summary>
+    /// Cumulative beam probability (renormalized relative to all beams)
+    /// </summary>
+    public readonly float CumulativeProbability;
+
+    /// <summary>
+    /// Callback should set this to true when a beam is at end-of-beam.
+    /// </summary>
+    public bool EndOfBeam;
+
+    /// <summary>
+    /// Tokens in this beam
+    /// </summary>
+    public readonly Span<llama_token> Tokens
+    {
+        get
+        {
+            unsafe
+            {
+                if (n_tokens > int.MaxValue)
+                    throw new InvalidOperationException("More than 2147483647 tokens is not supported");
+                return new Span<llama_token>(tokens, (int)n_tokens);
+            }
+        }
+    }
+}
---
+++ LLama/Native/LLamaBeamsState.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LLama.Native
+{
+    /// <summary>
+    /// Passed to beam_search_callback function.
+    /// Whenever 0 &lt; common_prefix_length, this number of tokens should be copied from any of the beams
+    /// (e.g. beams[0]) as they will be removed (shifted) from all beams in all subsequent callbacks.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public readonly struct LLamaBeamsState
+    {
+        /// <summary>
+        /// The state of each individual beam
+        /// </summary>
+        private readonly unsafe LLamaBeamView* beam_views;
+
+        /// <summary>
+        /// Number of elements in beam_views
+        /// </summary>
+        private readonly nint n_beams;
+
+        /// <summary>
+        /// Current max length of prefix tokens shared by all beams.
+        /// </summary>
+        public readonly ulong CommonPrefixLength;
+
+        /// <summary>
+        /// True iff this is the last callback invocation.
+        /// </summary>
+        public readonly bool LastCall;
+
+        /// <summary>
+        /// The current state of each beam
+        /// </summary>
+        public readonly Span<LLamaBeamView> BeamViews
+        {
+            get
+            {
+                unsafe
+                {
+                    if (n_beams > int.MaxValue)
+                        throw new InvalidOperationException("More than 2147483647 beams is not supported");
+                    return new Span<LLamaBeamView>(beam_views, (int)n_beams);
+                }
+            }
+        }
+    }
+}
