a2e29d393c1d23c9b1e163087815e206203083e3
Martin Evans <martindevans@gmail.com>
2024-01-20 21:18:35 +0000

    Swapped `StatelessExecutor` to use `llama_decode`!  - Added `logits_i` argument to `Context.ApplyPenalty`  - Added a new exception type for `llama_decode` return code

---
+++ LLama/Native/DecodeResult.cs
@@ -0,0 +1,22 @@
+﻿namespace LLama.Native;
+
+/// <summary>
+/// Return codes from llama_decode
+/// </summary>
+public enum DecodeResult
+{
+    /// <summary>
+    /// An unspecified error
+    /// </summary>
+    Error = -1,
+
+    /// <summary>
+    /// Ok.
+    /// </summary>
+    Ok = 0,
+
+    /// <summary>
+    /// Could not find a KV slot for the batch (try reducing the size of the batch or increase the context)
+    /// </summary>
+    NoKvSlot = 1,
+}
