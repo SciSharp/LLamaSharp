91ca9d2732a178d4207a7a663fcc17b789ddc4ae
Scott W Harden <swharden@gmail.com>
2024-02-22 19:44:06 -0500

    LLamaSharp.Examples: Document Q&A with local storage (#532)
    * LLama.Examples: disable console logging
    
    * LLama.Examples: rename titles to signal grouped topics
    
    * LLama.Examples: add additional PDF for Q&A
    
    * LLama.Examples: improve kernel memory demo
    
    multi-document ingestion
    
    * LLama.Examples: improve message before resetting to main menu
    
    * LLama.Examples: document Q&A with local memory

--- LLama.Examples/Examples/KernelMemory.cs
+++ LLama.Examples/Examples/KernelMemory.cs
@@ -1,58 +1,110 @@
 ﻿using LLamaSharp.KernelMemory;
 using Microsoft.KernelMemory;
 using Microsoft.KernelMemory.Configuration;
+using System.Diagnostics;
 
 namespace LLama.Examples.Examples
 {
+    // This example is from Microsoft's official kernel memory "custom prompts" example:
+    // https://github.com/microsoft/kernel-memory/blob/6d516d70a23d50c6cb982e822e6a3a9b2e899cfa/examples/101-dotnet-custom-Prompts/Program.cs#L1-L86
+
+    // Microsoft.KernelMemory has more features than Microsoft.SemanticKernel.
+    // See https://microsoft.github.io/kernel-memory/ for details.
+
     public class KernelMemory
     {
         public static async Task Run()
         {
-            string modelPath = UserSettings.GetModelPath();
-
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("This example is from : \n" +
-                "https://github.com/microsoft/kernel-memory/blob/main/examples/101-using-core-nuget/Program.cs");
+            Console.WriteLine(
+                """
 
-            var searchClientConfig = new SearchClientConfig
+                This program uses the Microsoft.KernelMemory package to ingest documents
+                and answer questions about them in an interactive chat prompt.
+
+                """);
+
+            // Setup the kernel memory with the LLM model
+            string modelPath = UserSettings.GetModelPath();
+            IKernelMemory memory = CreateMemory(modelPath);
+
+            // Ingest documents (format is automatically detected from the filename)
+            string[] filesToIngest = [
+                Path.GetFullPath(@"./Assets/sample-SK-Readme.pdf"),
+                Path.GetFullPath(@"./Assets/sample-KM-Readme.pdf"),
+            ];
+
+            for (int i = 0; i < filesToIngest.Length; i++)
+            {
+                string path = filesToIngest[i];
+                Stopwatch sw = Stopwatch.StartNew();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"Importing {i + 1} of {filesToIngest.Length}: {path}");
+                await memory.ImportDocumentAsync(path, steps: Constants.PipelineWithoutSummary);
+                Console.WriteLine($"Completed in {sw.Elapsed}\n");
+            }
+
+            // Ask a predefined question
+            Console.ForegroundColor = ConsoleColor.Green;
+            string question1 = "What formats does KM support";
+            Console.WriteLine($"Question: {question1}");
+            await AnswerQuestion(memory, question1);
+
+            // Let the user ask additional questions
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Question: ");
+                string question = Console.ReadLine()!;
+                if (string.IsNullOrEmpty(question))
+                    return;
+
+                await AnswerQuestion(memory, question);
+            }
+        }
+
+        private static IKernelMemory CreateMemory(string modelPath)
+        {
+            Common.InferenceParams infParams = new() { AntiPrompts = ["\n\n"] };
+
+            LLamaSharpConfig lsConfig = new(modelPath) { DefaultInferenceParams = infParams };
+
+            SearchClientConfig searchClientConfig = new()
             {
                 MaxMatchesCount = 1,
                 AnswerTokens = 100,
             };
 
-            var memory = new KernelMemoryBuilder()
-                    .WithLLamaSharpDefaults(new LLamaSharpConfig(modelPath)
-                    {
-                        DefaultInferenceParams = new Common.InferenceParams
-                        {
-                            AntiPrompts = new List<string> { "\n\n" }
-                        }
-                    })
-                    .WithSearchClientConfig(searchClientConfig)
-                    .With(new TextPartitioningOptions
-                    {
-                        MaxTokensPerParagraph = 300,
-                        MaxTokensPerLine = 100,
-                        OverlappingTokens = 30
-                    })
-                .Build();
-
-            await memory.ImportDocumentAsync(@"./Assets/sample-SK-Readme.pdf", steps: Constants.PipelineWithoutSummary);
-
-            var question = "What's Semantic Kernel?";
-
-            Console.WriteLine($"\n\nQuestion: {question}");
-
-            var answer = await memory.AskAsync(question);
-
-            Console.WriteLine($"\nAnswer: {answer.Result}");
-
-            Console.WriteLine("\n\n  Sources:\n");
-
-            foreach (var x in answer.RelevantSources)
+            TextPartitioningOptions parseOptions = new()
             {
-                Console.WriteLine($"  - {x.SourceName}  - {x.Link} [{x.Partitions.First().LastUpdate:D}]");
+                MaxTokensPerParagraph = 300,
+                MaxTokensPerLine = 100,
+                OverlappingTokens = 30
+            };
+
+            return new KernelMemoryBuilder()
+                .WithLLamaSharpDefaults(lsConfig)
+                .WithSearchClientConfig(searchClientConfig)
+                .With(parseOptions)
+                .Build();
+        }
+
+        private static async Task AnswerQuestion(IKernelMemory memory, string question)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine($"Generating answer...");
+
+            MemoryAnswer answer = await memory.AskAsync(question);
+            Console.WriteLine($"Answer generated in {sw.Elapsed}");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($"Answer: {answer.Result}");
+            foreach (var source in answer.RelevantSources)
+            {
+                Console.WriteLine($"Source: {source.SourceName}");
             }
+            Console.WriteLine();
         }
     }
 }
---
+++ LLama.Examples/Examples/KernelMemorySaveAndLoad.cs
@@ -0,0 +1,161 @@
+﻿using LLamaSharp.KernelMemory;
+using Microsoft.KernelMemory;
+using Microsoft.KernelMemory.Configuration;
+using Microsoft.KernelMemory.ContentStorage.DevTools;
+using Microsoft.KernelMemory.FileSystem.DevTools;
+using Microsoft.KernelMemory.MemoryStorage.DevTools;
+using System.Diagnostics;
+
+namespace LLama.Examples.Examples;
+
+public class KernelMemorySaveAndLoad
+{
+    static string StorageFolder => Path.GetFullPath($"./storage-{nameof(KernelMemorySaveAndLoad)}");
+    static bool StorageExists => Directory.Exists(StorageFolder) && Directory.GetDirectories(StorageFolder).Length > 0;
+
+    public static async Task Run()
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(
+            """
+
+            This program uses the Microsoft.KernelMemory package to ingest documents
+            and store the embeddings as local files so they can be quickly recalled
+            when this application is launched again. 
+
+            """);
+
+        string modelPath = UserSettings.GetModelPath();
+        IKernelMemory memory = CreateMemoryWithLocalStorage(modelPath);
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        if (StorageExists)
+        {
+            Console.WriteLine(
+                """
+                
+                Kernel memory files have been located!
+                Information about previously analyzed documents has been loaded.
+
+                """);
+        }
+        else
+        {
+            Console.WriteLine(
+                $"""
+
+                 Existing kernel memory was not found.
+                 Documents will be analyzed (slow) and information saved to disk.
+                 Analysis will not be required the next time this program is run.
+                 Press ENTER to proceed...
+ 
+                 """);
+            Console.ReadLine();
+            await IngestDocuments(memory);
+        }
+
+        await AskSingleQuestion(memory, "What formats does KM support?");
+        await StartUserChatSession(memory);
+    }
+
+    private static IKernelMemory CreateMemoryWithLocalStorage(string modelPath)
+    {
+        Common.InferenceParams infParams = new() { AntiPrompts = ["\n\n"] };
+
+        LLamaSharpConfig lsConfig = new(modelPath) { DefaultInferenceParams = infParams };
+
+        SearchClientConfig searchClientConfig = new()
+        {
+            MaxMatchesCount = 1,
+            AnswerTokens = 100,
+        };
+
+        TextPartitioningOptions parseOptions = new()
+        {
+            MaxTokensPerParagraph = 300,
+            MaxTokensPerLine = 100,
+            OverlappingTokens = 30
+        };
+
+        SimpleFileStorageConfig storageConfig = new()
+        {
+            Directory = StorageFolder,
+            StorageType = FileSystemTypes.Disk,
+        };
+
+        SimpleVectorDbConfig vectorDbConfig = new()
+        {
+            Directory = StorageFolder,
+            StorageType = FileSystemTypes.Disk,
+        };
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"Kernel memory folder: {StorageFolder}");
+
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        return new KernelMemoryBuilder()
+            .WithSimpleFileStorage(storageConfig)
+            .WithSimpleVectorDb(vectorDbConfig)
+            .WithLLamaSharpDefaults(lsConfig)
+            .WithSearchClientConfig(searchClientConfig)
+            .With(parseOptions)
+            .Build();
+    }
+
+    private static async Task AskSingleQuestion(IKernelMemory memory, string question)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Question: {question}");
+        await ShowAnswer(memory, question);
+    }
+
+    private static async Task StartUserChatSession(IKernelMemory memory)
+    {
+        while (true)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Question: ");
+            string question = Console.ReadLine()!;
+            if (string.IsNullOrEmpty(question))
+                return;
+
+            await ShowAnswer(memory, question);
+        }
+    }
+
+    private static async Task IngestDocuments(IKernelMemory memory)
+    {
+        string[] filesToIngest = [
+                Path.GetFullPath(@"./Assets/sample-SK-Readme.pdf"),
+                Path.GetFullPath(@"./Assets/sample-KM-Readme.pdf"),
+            ];
+
+        for (int i = 0; i < filesToIngest.Length; i++)
+        {
+            string path = filesToIngest[i];
+            Stopwatch sw = Stopwatch.StartNew();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Importing {i + 1} of {filesToIngest.Length}: {path}");
+            await memory.ImportDocumentAsync(path, steps: Constants.PipelineWithoutSummary);
+            Console.WriteLine($"Completed in {sw.Elapsed}\n");
+        }
+    }
+
+    private static async Task ShowAnswer(IKernelMemory memory, string question)
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        Console.WriteLine($"Generating answer...");
+
+        MemoryAnswer answer = await memory.AskAsync(question);
+        Console.WriteLine($"Answer generated in {sw.Elapsed}");
+
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine($"Answer: {answer.Result}");
+        foreach (var source in answer.RelevantSources)
+        {
+            Console.WriteLine($"Source: {source.SourceName}");
+        }
+        Console.WriteLine();
+    }
+}
