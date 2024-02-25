91a7967869ec1d3441e9b819bc164d178d772b96
Martin Evans <martindevans@gmail.com>
2024-02-25 02:12:00 +0000

    `ReadOnlySpan<float>` in ISamplingPipeline (#538)
    * - Modified ISamplingPipeline to accept `ReadOnlySpan<float>` of logits directly. This moves responsibility to copy the logits into the pipeline.
     - Added a flag to `BaseSamplingPipeline` indicating if a logit copy is necessary. Skipping it in most cases.
    
    * Fixed `RestoreProtectedTokens` not working if logit processing is skipped
    
    * - Implemented a new greedy sampling pipeline (always sample most likely token)
     - Moved `Grammar` into `BaseSamplingPipeline`
     - Removed "protected tokens" concept from `BaseSamplingPipeline`. Was introducing a lot of incidental complexity.
     - Implemented newline logit save/restore in `DefaultSamplingPipeline` (only place protected tokens was used)
    
    * Implemented pipelines for mirostat v1 and v2

--- LLama/Sampling/BaseSamplingPipeline.cs
+++ LLama/Sampling/BaseSamplingPipeline.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Buffers;
-using System.Collections.Generic;
 using LLama.Native;
 
 namespace LLama.Sampling;
@@ -11,84 +9,28 @@ namespace LLama.Sampling;
 public abstract class BaseSamplingPipeline
     : ISamplingPipeline
 {
-    private int _savedLogitsCount;
-    private (LLamaToken index, float logit)[]? _savedLogits;
+    /// <summary>
+    /// Grammar to constrain valid tokens
+    /// </summary>
+    public SafeLLamaGrammarHandle? Grammar { get; set; }
 
     /// <inheritdoc/>
-    public LLamaToken Sample(SafeLLamaContextHandle ctx, Span<float> logits, ReadOnlySpan<LLamaToken> lastTokens)
+    public LLamaToken Sample(SafeLLamaContextHandle ctx, ReadOnlySpan<float> logits, ReadOnlySpan<LLamaToken> lastTokens)
     {
-        var protectedLogits = GetProtectedTokens(ctx);
-        _savedLogitsCount = protectedLogits.Count;
-        _savedLogits = ArrayPool<(LLamaToken, float)>.Shared.Rent(_savedLogitsCount);
-        try
-        {
-            // Save the values of protected logits
-            for (var i = 0; i < protectedLogits.Count; i++)
-            {
-                var index = protectedLogits[i];
-                var value = logits[(int)index];
-                _savedLogits[i] = (index, value);
-            }
+        // Apply processing to raw logit values
+        logits = ProcessLogits(ctx, logits, lastTokens);
 
-            // Process raw logits
-            ProcessLogits(ctx, logits, lastTokens);
-
-            // Automatically restore saved logit values after processing
-            RestoreProtectedTokens(logits);
-
-            // Convert logits into token candidates
-            var candidates = LLamaTokenDataArray.Create(logits);
-
-            // Process token data array
-            return ProcessTokenDataArray(ctx, candidates, lastTokens);
-        }
-        finally
-        {
-            ArrayPool<(LLamaToken, float)>.Shared.Return(_savedLogits);
-            _savedLogits = null;
-            _savedLogitsCount = 0;
-        }
+        // Process token data array to select a final token
+        var candidates = LLamaTokenDataArray.Create(logits);
+        candidates.ApplyGrammar(ctx, Grammar);
+        return ProcessTokenDataArray(ctx, candidates, lastTokens);
     }
 
     /// <inheritdoc />
-    public abstract void Accept(SafeLLamaContextHandle ctx, LLamaToken token);
-
-    #region protected tokens
-    /// <summary>
-    /// Get all of the "protected" tokens that cannot be changed by ProcessLogits
-    /// </summary>
-    /// <returns></returns>
-    protected abstract IReadOnlyList<LLamaToken> GetProtectedTokens(SafeLLamaContextHandle ctx);
-
-    /// <summary>
-    /// Restore the value of the "protected" tokens which were saved before the call to ProcessLogits
-    /// </summary>
-    /// <param name="logits"></param>
-    protected void RestoreProtectedTokens(Span<float> logits)
+    public virtual void Accept(SafeLLamaContextHandle ctx, LLamaToken token)
     {
-        if (_savedLogits == null)
-            return;
-
-        // The array may be bigger than necessary, get a span of the valid bit
-        var saved = _savedLogits.AsSpan(0, _savedLogitsCount);
-
-        // Restore the values of protected logits
-        for (var i = 0; i < saved.Length; i++)
-            logits[(int)saved[i].index] = saved[i].logit;
+        Grammar?.AcceptToken(ctx, token);
     }
-
-    /// <summary>
-    /// Restore the value of the "protected" tokens which were saved before the call to ProcessLogits
-    /// </summary>
-    /// <param name="candidates"></param>
-    protected void RestoreProtectedTokens(LLamaTokenDataArray candidates)
-    {
-        if (_savedLogits == null || _savedLogits.Length == 0)
-            return;
-
-        candidates.OverwriteLogits(_savedLogits.AsSpan(0, _savedLogitsCount));
-    }
-    #endregion
 
     /// <summary>
     /// Process the raw logit values
@@ -96,7 +38,7 @@ public abstract class BaseSamplingPipeline
     /// <param name="ctx">The context being sampled from</param>
     /// <param name="logits">The logits produced by the model</param>
     /// <param name="lastTokens">A list of tokens recently returned by the model</param>
-    protected abstract void ProcessLogits(SafeLLamaContextHandle ctx, Span<float> logits, ReadOnlySpan<LLamaToken> lastTokens);
+    protected abstract ReadOnlySpan<float> ProcessLogits(SafeLLamaContextHandle ctx, ReadOnlySpan<float> logits, ReadOnlySpan<LLamaToken> lastTokens);
 
     /// <summary>
     /// Process the LLamaTokenDataArray and select a single token
