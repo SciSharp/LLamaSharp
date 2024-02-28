a0731db944775d91e5972516536283fa853fbf5d
Martin Evans <martindevans@gmail.com>
2024-02-28 17:12:24 +0000

    Added tests checking that memory is freed properly (#551)

---
+++ LLama.Unittest/MemoryDisposalTests.cs
@@ -0,0 +1,42 @@
+﻿using LLama.Common;
+
+namespace LLama.Unittest;
+
+public class MemoryDisposalTests
+{
+    [Fact]
+    public void ModelDisposal()
+    {
+        var @params = new ModelParams(Constants.ModelPath)
+        {
+            ContextSize = 2048
+        };
+        var model = LLamaWeights.LoadFromFile(@params);
+
+        Assert.False(model.NativeHandle.IsClosed);
+        model.Dispose();
+        Assert.True(model.NativeHandle.IsClosed);
+    }
+
+    [Fact]
+    public void ContextDisposal()
+    {
+        var @params = new ModelParams(Constants.ModelPath)
+        {
+            ContextSize = 2048
+        };
+        var model = LLamaWeights.LoadFromFile(@params);
+
+        var ctx = model.CreateContext(@params);
+
+        // Disposing the model handle does **not** free the memory, because there's stilla context
+        Assert.False(model.NativeHandle.IsClosed);
+        model.Dispose();
+        Assert.False(model.NativeHandle.IsClosed);
+
+        // Disposing the context frees context and model weights
+        ctx.Dispose();
+        Assert.True(model.NativeHandle.IsClosed);
+        Assert.True(ctx.NativeHandle.IsClosed);
+    }
+}
