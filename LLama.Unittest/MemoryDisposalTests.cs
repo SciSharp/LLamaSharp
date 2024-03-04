ec4d8f14970c06818e48b4e1ec3ddc46c7135e4d
Zoli Somogyi <zsomogyi.be@gmail.com>
2024-03-04 21:06:25 +0100

    GPU Memory Leak test
    There is a GPU memory leak in LLamaSharp. This test extension shows the amount of memory which is not freed. This will help to trace the problem.

--- LLama.Unittest/MemoryDisposalTests.cs
+++ LLama.Unittest/MemoryDisposalTests.cs
@@ -1,12 +1,39 @@
 ﻿using LLama.Common;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace LLama.Unittest;
 
 public class MemoryDisposalTests
 {
+    [DllImport("llama", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int ggml_backend_cuda_get_device_count();
+
+    [DllImport("llama", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void ggml_backend_cuda_get_device_description(int device, StringBuilder description, ulong description_size);
+
+    [DllImport("llama", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void ggml_backend_cuda_get_device_memory(int device, out ulong free, out ulong total);
+
+    private static ulong GetGPUFreeMemory()
+    {
+        ulong freemem = 0;
+        int dc = ggml_backend_cuda_get_device_count();
+        if (dc > 0)
+        {
+            ulong totalmem = 0;
+            StringBuilder sb = new StringBuilder(1024 * 1024);
+            ggml_backend_cuda_get_device_description(0, sb, 1024 * 1024);
+            ggml_backend_cuda_get_device_memory(0, out freemem, out totalmem);
+        }
+        return freemem;
+    }
+
     [Fact]
     public void ModelDisposal()
     {
+        ulong memS = GetGPUFreeMemory();
+
         var @params = new ModelParams(Constants.ModelPath)
         {
             ContextSize = 2048
@@ -16,11 +43,19 @@ public class MemoryDisposalTests
         Assert.False(model.NativeHandle.IsClosed);
         model.Dispose();
         Assert.True(model.NativeHandle.IsClosed);
+
+        if (memS > 0)
+        {
+            ulong memE = GetGPUFreeMemory();
+            Assert.True(memE >= memS);
+        }
     }
 
     [Fact]
     public void ContextDisposal()
     {
+        ulong memS = GetGPUFreeMemory();
+
         var @params = new ModelParams(Constants.ModelPath)
         {
             ContextSize = 2048
@@ -38,5 +73,11 @@ public class MemoryDisposalTests
         ctx.Dispose();
         Assert.True(model.NativeHandle.IsClosed);
         Assert.True(ctx.NativeHandle.IsClosed);
+
+        if (memS > 0)
+        {
+            ulong memE = GetGPUFreeMemory();
+            Assert.True(memE >= memS);
+        }
     }
 }
