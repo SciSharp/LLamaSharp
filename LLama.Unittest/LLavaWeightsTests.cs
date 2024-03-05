0110745b1189c8656a17389bf18235c85ffc16dd
SignalRT <admin@signalrt.com>
2024-03-05 06:52:39 +0100

    Cuda and Cblast binaries

--- LLama.Unittest/LLavaWeightsTests.cs
+++ LLama.Unittest/LLavaWeightsTests.cs
@@ -32,69 +32,7 @@ namespace LLama.Unittest
             _lLavaWeights.Dispose();
         }
 
-        // [Fact]
-        // public void CheckProperties()
-        // {
-        //     Assert.Equal(4096, (int)_context.ContextSize);
-        //     Assert.Equal(4096, _context.EmbeddingSize);
-        //     Assert.Equal(32000, _context.VocabCount);
-        // }
-
-        /*[Fact]
-        public void Tokenize()
-        {
-            var tokens = _context.Tokenize("The quick brown fox", true);
-
-            Assert.Equal(new LLamaToken[] { 1, 450, 4996, 17354, 1701, 29916 }, tokens);
-        }
-
-        [Fact]
-        public void TokenizeNewline()
-        {
-            var tokens = _context.Tokenize("\n", false, false);
-
-            Assert.Equal(new LLamaToken[] { 29871, 13 }, tokens);
-        }
-
-        [Fact]
-        public void TokenizeRoundtripSpecialStrings()
-        {
-            var strings = new[]
-            {
-                "\t", "\t\t", "\t\t\t",
-                "\n\n", "\n\n\n", "\n\n\n\n",
-                "\t\n", "\t\n\t\n\n\n\n\t\t",
-                "\b", "\v", "\0"
-            };
-
-            foreach (var s in strings)
-            {
-                var tokens = _context.Tokenize(s, false, false);
-                var decoder = new StreamingTokenDecoder(_context);
-                decoder.AddRange(tokens);
-
-                var str = decoder.Read();
-
-                Assert.Equal(s, str.TrimStart(' '));
-            }
-        }
-
-        [Fact]
-        public void TokenizeWithoutBOS()
-        {
-            var tokens = _context.Tokenize("The quick brown fox", false);
-
-            Assert.Equal(new LLamaToken[] { 450, 4996, 17354, 1701, 29916 }, tokens);
-        }
-
-        [Fact]
-        public void TokenizeEmpty()
-        {
-            var tokens = _context.Tokenize("", false);
-
-            Assert.Equal(Array.Empty<LLamaToken>(), tokens);
-        }*/
-        
+      
         
         [Fact]
         public void EmbedImageAsFileName()
