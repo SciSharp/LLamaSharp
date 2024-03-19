9d1db776d1e533d60321bf6ef6cfd394d449326e
Martin Evans <martindevans@gmail.com>
2024-03-19 00:15:55 +0000

    Added a list of active conversations to the batched executor.

--- LLama/Batched/BatchedExecutor.cs
+++ LLama/Batched/BatchedExecutor.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using LLama.Abstractions;
 using LLama.Native;
+using System.Collections.Generic;
 
 namespace LLama.Batched;
 
@@ -13,6 +14,12 @@ public sealed class BatchedExecutor
     : IDisposable
 {
     private int _nextSequenceId;
+
+    /// <summary>
+    /// Store a reference to conversations created from this executor. Storing them as weak references allows
+    /// the garbage collector to clean them up if Dispose is not properly called.
+    /// </summary>
+    private readonly List<WeakReference<Conversation>> _conversations = new();
 
     internal LLamaBatch Batch { get; }
 
@@ -36,6 +43,18 @@ public sealed class BatchedExecutor
     /// Get the number of tokens in the batch, waiting for <see cref="Infer"/> to be called
     /// </summary>
     public int BatchedTokenCount => Batch.TokenCount;
+
+    /// <summary>
+    /// Get the number of conversations in this batch
+    /// </summary>
+    public int ConversationCount
+    {
+        get
+        {
+            TrimConversationReferences();
+            return _conversations.Count;
+        }
+    }
 
     /// <summary>
     /// Check if this executor has been disposed.
@@ -64,6 +83,16 @@ public sealed class BatchedExecutor
     }
 
     /// <summary>
+    /// Remove all conversation references which are no longer valid
+    /// </summary>
+    private void TrimConversationReferences()
+    {
+        for (var i = _conversations.Count - 1; i >= 0; i--)
+            if (!_conversations[i].TryGetTarget(out var c) || c.IsDisposed)
+                _conversations.RemoveAt(i);
+    }
+
+    /// <summary>
     /// Start a new <see cref="Conversation"/> with the given prompt
     /// </summary>
     /// <param name="prompt"></param>
@@ -89,7 +118,12 @@ public sealed class BatchedExecutor
         if (IsDisposed)
             throw new ObjectDisposedException(nameof(BatchedExecutor));
 
-        return new Conversation(this, GetNextSequenceId(), 0);
+        var conversation = new Conversation(this, GetNextSequenceId(), 0);
+
+        TrimConversationReferences();
+        _conversations.Add(new WeakReference<Conversation>(conversation));
+
+        return conversation;
     }
 
     /// <summary>
