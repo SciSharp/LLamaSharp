1fbdc21555f81e17fc2df6c3c80ac1f8bd36ab9c
Michael Lamothe <michael.lamothe@gmail.com>
2024-05-06 16:57:35 +1000

    * Updated to .NET 8 * Updated to Bootstrap 5.3.3 * Use libman for client side packages * Enabled razor runtime compilation in dev * Added fav icon * Removed the footer * Added theme switcher

--- LLama.Web/Common/ModelOptions.cs
+++ LLama.Web/Common/ModelOptions.cs
@@ -1,117 +1,116 @@
-﻿using System.Text;
+using System.Text;
 using LLama.Abstractions;
 using LLama.Native;
 
-namespace LLama.Web.Common
+namespace LLama.Web.Common;
+
+public class ModelOptions
+    : ILLamaParams
 {
-    public class ModelOptions
-        : ILLamaParams
-    {
-        /// <summary>
-        /// Model friendly name
-        /// </summary>
-        public string Name { get; set; }
+    /// <summary>
+    /// Model friendly name
+    /// </summary>
+    public string Name { get; set; }
 
-        /// <summary>
-        /// Max context insta=nces allowed per model
-        /// </summary>
-        public int MaxInstances { get; set; }
+    /// <summary>
+    /// Max context insta=nces allowed per model
+    /// </summary>
+    public int MaxInstances { get; set; }
 
-        /// <inheritdoc />
-        public uint? ContextSize { get; set; }
+    /// <inheritdoc />
+    public uint? ContextSize { get; set; }
 
-        /// <inheritdoc />
-        public int MainGpu { get; set; } = 0;
+    /// <inheritdoc />
+    public int MainGpu { get; set; } = 0;
 
-        /// <inheritdoc />
-        public GPUSplitMode SplitMode { get; set; } = GPUSplitMode.None;
+    /// <inheritdoc />
+    public GPUSplitMode SplitMode { get; set; } = GPUSplitMode.None;
 
-        /// <inheritdoc />
-        public int GpuLayerCount { get; set; } = 20;
+    /// <inheritdoc />
+    public int GpuLayerCount { get; set; } = 20;
 
-        public uint SeqMax { get; }
+    public uint SeqMax { get; }
 
-        /// <inheritdoc />
-        public uint Seed { get; set; } = 1686349486;
+    /// <inheritdoc />
+    public uint Seed { get; set; } = 1686349486;
 
-        public bool Embeddings { get; }
+    public bool Embeddings { get; }
 
-        /// <inheritdoc />
-        public bool UseMemorymap { get; set; } = true;
+    /// <inheritdoc />
+    public bool UseMemorymap { get; set; } = true;
 
-        /// <inheritdoc />
-        public bool UseMemoryLock { get; set; } = false;
+    /// <inheritdoc />
+    public bool UseMemoryLock { get; set; } = false;
 
-        /// <inheritdoc />
-        public string ModelPath { get; set; }
+    /// <inheritdoc />
+    public string ModelPath { get; set; }
 
-        /// <inheritdoc />
-        public AdapterCollection LoraAdapters { get; set; } = new();
+    /// <inheritdoc />
+    public AdapterCollection LoraAdapters { get; set; } = new();
 
-        /// <inheritdoc />
-        public string LoraBase { get; set; } = string.Empty;
+    /// <inheritdoc />
+    public string LoraBase { get; set; } = string.Empty;
 
-        /// <inheritdoc />
-        public uint? Threads { get; set; }
+    /// <inheritdoc />
+    public uint? Threads { get; set; }
 
-        /// <inheritdoc />
-        public uint? BatchThreads { get; set; }
+    /// <inheritdoc />
+    public uint? BatchThreads { get; set; }
 
-        /// <inheritdoc />
-        public uint BatchSize { get; set; } = 512;
+    /// <inheritdoc />
+    public uint BatchSize { get; set; } = 512;
 
-        /// <inheritdoc />
-        public uint UBatchSize { get; set; } = 512;
+    /// <inheritdoc />
+    public uint UBatchSize { get; set; } = 512;
 
-        /// <inheritdoc />
-        public TensorSplitsCollection TensorSplits { get; set; } = new();
+    /// <inheritdoc />
+    public TensorSplitsCollection TensorSplits { get; set; } = new();
 
-        /// <inheritdoc />
-        public List<MetadataOverride> MetadataOverrides { get; } = new();
+    /// <inheritdoc />
+    public List<MetadataOverride> MetadataOverrides { get; } = new();
 
-        /// <inheritdoc />
-        public float? RopeFrequencyBase { get; set; }
+    /// <inheritdoc />
+    public float? RopeFrequencyBase { get; set; }
 
-        /// <inheritdoc />
-        public float? RopeFrequencyScale { get; set; }
+    /// <inheritdoc />
+    public float? RopeFrequencyScale { get; set; }
 
-        /// <inheritdoc />
-        public float? YarnExtrapolationFactor { get; set; }
+    /// <inheritdoc />
+    public float? YarnExtrapolationFactor { get; set; }
 
-        /// <inheritdoc />
-        public float? YarnAttentionFactor { get; set; }
+    /// <inheritdoc />
+    public float? YarnAttentionFactor { get; set; }
 
-        /// <inheritdoc />
-        public float? YarnBetaFast { get; set; }
+    /// <inheritdoc />
+    public float? YarnBetaFast { get; set; }
 
-        /// <inheritdoc />
-        public float? YarnBetaSlow { get; set; }
+    /// <inheritdoc />
+    public float? YarnBetaSlow { get; set; }
 
-        /// <inheritdoc />
-        public uint? YarnOriginalContext { get; set; }
+    /// <inheritdoc />
+    public uint? YarnOriginalContext { get; set; }
 
-        /// <inheritdoc />
-        public RopeScalingType? YarnScalingType { get; set; }
+    /// <inheritdoc />
+    public RopeScalingType? YarnScalingType { get; set; }
 
-        /// <inheritdoc />
-        public GGMLType? TypeK { get; set; }
+    /// <inheritdoc />
+    public GGMLType? TypeK { get; set; }
 
-        /// <inheritdoc />
-        public GGMLType? TypeV { get; set; }
+    /// <inheritdoc />
+    public GGMLType? TypeV { get; set; }
 
-        /// <inheritdoc />
-        public bool NoKqvOffload { get; set; }
+    /// <inheritdoc />
+    public bool NoKqvOffload { get; set; }
 
-        /// <inheritdoc />
-        public Encoding Encoding { get; set; } = Encoding.UTF8;
+    /// <inheritdoc />
+    public Encoding Encoding { get; set; } = Encoding.UTF8;
 
-        /// <inheritdoc />
-        public bool VocabOnly { get; set; }
+    /// <inheritdoc />
+    public bool VocabOnly { get; set; }
 
-        /// <inheritdoc />
-        public float DefragThreshold { get; set; }
+    /// <inheritdoc />
+    public float DefragThreshold { get; set; }
 
-        /// <inheritdoc />
-        public LLamaPoolingType PoolingType { get; set; }
-    }
+    /// <inheritdoc />
+    public LLamaPoolingType PoolingType { get; set; }
 }
--- LLama.Web/Hubs/SessionConnectionHub.cs
+++ LLama.Web/Hubs/SessionConnectionHub.cs
@@ -1,67 +1,63 @@
-﻿using LLama.Web.Common;
+using LLama.Web.Common;
 using LLama.Web.Models;
 using LLama.Web.Services;
 using Microsoft.AspNetCore.SignalR;
 
-namespace LLama.Web.Hubs
+namespace LLama.Web.Hubs;
+
+public class SessionConnectionHub : Hub<ISessionClient>
 {
-    public class SessionConnectionHub : Hub<ISessionClient>
+    private readonly ILogger<SessionConnectionHub> _logger;
+    private readonly IModelSessionService _modelSessionService;
+
+    public SessionConnectionHub(ILogger<SessionConnectionHub> logger, IModelSessionService modelSessionService)
     {
-        private readonly ILogger<SessionConnectionHub> _logger;
-        private readonly IModelSessionService _modelSessionService;
+        _logger = logger;
+        _modelSessionService = modelSessionService;
+    }
 
-        public SessionConnectionHub(ILogger<SessionConnectionHub> logger, IModelSessionService modelSessionService)
+    public override async Task OnConnectedAsync()
+    {
+        _logger.Log(LogLevel.Information, "[OnConnectedAsync], Id: {0}", Context.ConnectionId);
+
+        // Notify client of successful connection
+        await Clients.Caller.OnStatus(Context.ConnectionId, SessionConnectionStatus.Connected);
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception exception)
+    {
+        _logger.Log(LogLevel.Information, "[OnDisconnectedAsync], Id: {0}", Context.ConnectionId);
+
+        // Remove connections session on disconnect
+        await _modelSessionService.CloseAsync(Context.ConnectionId);
+        await base.OnDisconnectedAsync(exception);
+    }
+
+    [HubMethodName("LoadModel")]
+    public async Task OnLoadModel(SessionConfig sessionConfig, InferenceOptions inferenceConfig)
+    {
+        _logger.Log(LogLevel.Information, "[OnLoadModel] - Load new model, Connection: {0}", Context.ConnectionId);
+        await _modelSessionService.CloseAsync(Context.ConnectionId);
+
+        // Create model session
+        var modelSession = await _modelSessionService.CreateAsync(Context.ConnectionId, sessionConfig, inferenceConfig);
+        if (modelSession is null)
         {
-            _logger = logger;
-            _modelSessionService = modelSessionService;
+            await Clients.Caller.OnError("Failed to create model session");
+            return;
         }
 
-        public override async Task OnConnectedAsync()
-        {
-            _logger.Log(LogLevel.Information, "[OnConnectedAsync], Id: {0}", Context.ConnectionId);
+        // Notify client
+        await Clients.Caller.OnStatus(Context.ConnectionId, SessionConnectionStatus.Loaded);
+    }
 
-            // Notify client of successful connection
-            await Clients.Caller.OnStatus(Context.ConnectionId, SessionConnectionStatus.Connected);
-            await base.OnConnectedAsync();
-        }
+    [HubMethodName("SendPrompt")]
+    public IAsyncEnumerable<TokenModel> OnSendPrompt(string prompt, InferenceOptions inferConfig, CancellationToken cancellationToken)
+    {
+        _logger.Log(LogLevel.Information, "[OnSendPrompt] - New prompt received, Connection: {0}", Context.ConnectionId);
 
-
-        public override async Task OnDisconnectedAsync(Exception exception)
-        {
-            _logger.Log(LogLevel.Information, "[OnDisconnectedAsync], Id: {0}", Context.ConnectionId);
-
-            // Remove connections session on disconnect
-            await _modelSessionService.CloseAsync(Context.ConnectionId);
-            await base.OnDisconnectedAsync(exception);
-        }
-
-
-        [HubMethodName("LoadModel")]
-        public async Task OnLoadModel(SessionConfig sessionConfig, InferenceOptions inferenceConfig)
-        {
-            _logger.Log(LogLevel.Information, "[OnLoadModel] - Load new model, Connection: {0}", Context.ConnectionId);
-            await _modelSessionService.CloseAsync(Context.ConnectionId);
-
-            // Create model session
-            var modelSession = await _modelSessionService.CreateAsync(Context.ConnectionId, sessionConfig, inferenceConfig);
-            if (modelSession is null)
-            {
-                await Clients.Caller.OnError("Failed to create model session");
-                return;
-            }
-
-            // Notify client
-            await Clients.Caller.OnStatus(Context.ConnectionId, SessionConnectionStatus.Loaded);
-        }
-
-
-        [HubMethodName("SendPrompt")]
-        public IAsyncEnumerable<TokenModel> OnSendPrompt(string prompt, InferenceOptions inferConfig, CancellationToken cancellationToken)
-        {
-            _logger.Log(LogLevel.Information, "[OnSendPrompt] - New prompt received, Connection: {0}", Context.ConnectionId);
-
-            var linkedCancelationToken = CancellationTokenSource.CreateLinkedTokenSource(Context.ConnectionAborted, cancellationToken);
-            return _modelSessionService.InferAsync(Context.ConnectionId, prompt, inferConfig, linkedCancelationToken.Token);
-        }
+        var linkedCancelationToken = CancellationTokenSource.CreateLinkedTokenSource(Context.ConnectionAborted, cancellationToken);
+        return _modelSessionService.InferAsync(Context.ConnectionId, prompt, inferConfig, linkedCancelationToken.Token);
     }
 }
